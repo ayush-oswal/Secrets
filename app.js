f4daf18a91d7c8f6d22427027e6dad7b155d3a1c
ayush-oswal <ayushoswal2003@gmail.com>
2023-05-10 22:02:51 +0530

    final commit, includes Oauth 2.0 feature

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/*j shint esversion:6
+/*jshint esversion:6
 hashing is another way of making our website and user info secure, functions like md5 help us achieve it. => md5(password)--> returns hashed password
 salting is the process of adding random chars at the end of hash password and use bcrypt>>md5
 saltrounds specify the number of times salting is done to our hash password.
@@ -13,6 +13,11 @@ we dont need to require passport local as it is a dependency for passport-local-
 passport local mongoose automatically hashes and salts passwords when we plugin it into our db
 serialize and deserialize are only necessary if we are using sessions
 passport js comes with really usefull methods such as authenticate, isAuthenticated(), login, logout, register, etc
+Next level of authentication is Oauth 2.0, we can authenticate users by using their google profile.
+we recieve an access token which can be saved in our db. first npm i passport-google-oauth20 then go to googles developer
+console and create an application. Then we create credentials such as client id and client secrets.
+findOrCreate is not a mongoose method, its an option we can either implement it manually using findOne and Create
+or we can install findOrCreate pakage
 */
 const express = require('express')
 const bodyParser = require('body-parser')
@@ -21,6 +26,8 @@ const mongoose = require('mongoose')
 const session = require('express-session')
 const passport = require('passport')
 const passportLocalMongoose = require('passport-local-mongoose')
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 require("dotenv").config()
 const app = express()
 app.use(express.static("public"))
@@ -49,18 +56,49 @@ mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true, useUnifiedTop
 
 const userSchema = new mongoose.Schema ({
     email:String,
-    password:String
+    password:String,
+    googleId:String,
+    secret:String
 })
 
 
 userSchema.plugin(passportLocalMongoose)
+userSchema.plugin(findOrCreate)
 
 const User = mongoose.model("users",userSchema)
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, {
+        id: user.id,
+        username: user.username,
+        picture: user.picture
+      });
+    });
+  });
+  
+  passport.deserializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, user);
+    });
+  });
+
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile)
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 
 app.get("/",(req,res)=>{
@@ -76,12 +114,11 @@ app.get("/register",(req,res)=>{
 });
 
 app.get("/secrets",(req,res)=>{
-    if(req.isAuthenticated()){
-        res.render("secrets")
-    }
-    else{
-        res.redirect("/login")
-    }
+    User.find({"secret":{$ne:null}})
+    .then((foundusers)=>{
+        res.render("secrets",{secrets:foundusers})
+    })
+    .catch(err=>{console.log(err)})
 });
 
 app.get("/logout",(req,res)=>{
@@ -95,6 +132,26 @@ app.get("/logout",(req,res)=>{
     });
     
 }); 
+
+app.get("/submit",(req,res)=>{
+    if(req.isAuthenticated()){
+        res.render("submit")
+    }
+    else{
+        res.redirect("/login")
+    }
+});
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] })
+  );
+
+ app.get('/auth/google/secrets', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, render secrets page
+    res.redirect('/secrets');
+  });
 
 
 app.post("/register",(req,res)=>{
@@ -129,7 +186,17 @@ app.post("/login",(req,res)=>{
 });
 
 
-
+app.post("/submit",(req,res)=>{
+    const secret = req.body.secret
+    console.log(req.user.id)
+    User.findById(req.user.id)
+    .then((founduser)=>{
+        founduser.secret = secret
+        founduser.save()
+        res.redirect("/secrets")
+    })
+    .catch(err=>{console.log(err)})
+});
 
 
 
